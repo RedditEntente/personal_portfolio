07d8d452813fe815186e1e72d3937646a00764df
RedditEntente <110066993+RedditEntente@users.noreply.github.com>
2024-04-24 11:35:29 +1200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './routes/home';
 
 function App() {
   return ( <Routes>
-    <Route path="/" element = {<Home></Home>}></Route>
+    <Route path="/personal_portfolio" element = {<Home></Home>}></Route>
 
   </Routes>
   );
